dee0404a3ddee9267abcae3921ee1540d8a34f09
Marcus Browley Jr <marowley21@hotmail.com>
2022-09-23 01:38:56 -0700

    bored

---
+++ bored.py
@@ -0,0 +1,34 @@
+from colorizer import *
+from master import Master
+./api/activity?accessibility=:accessibility
+/api/activity?minaccessibility=:minaccessibility&maxaccessibility=:maxaccessibility
+
+def my_activity(self, master):
+    {
+	"activity": "Take a bubble bath",
+	"accessibility": 0.1,
+	"type": "relaxation",
+	"participants": 1,
+	"price": 0.15,
+}
+    while len(master.bored) < Mas:
+            print(Fore.BLUE if master.id % 2 == 0 else Fore.RED, Style.BRIGHT, end="")
+            clear_screen()
+            my_activity = input(f"{master.name} How about a Relaxing bubble bath? ")
+            if my_activity.lower() == "remove":
+                my_activity = input(f"{master.name} Already had a bubble bath? ").title()
+                if master.release(my_activity):
+                    print(f'lets get you something to do' {my_activity}')
+                    time.sleep(2)
+
+def my_activity2(self, master):
+    {
+	"activity":"Learn a new recipe",
+	"accessibility":0.05,
+	"type":"cooking",
+	"participants":1,
+	"price":0
+}
+/api/activity?accessibility=:accessibility
+
+main()
