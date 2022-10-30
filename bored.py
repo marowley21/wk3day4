d7c1a149f353cecc2b32fa1f6f9f2100aa54db82
Marcus Browley Jr <marowley21@hotmail.com>
2022-10-30 16:15:36 -0700

    updated

--- bored.py
+++ bored.py
@@ -1,34 +1,52 @@
-from colorizer import *
-from master import Master
-./api/activity?accessibility=:accessibility
-/api/activity?minaccessibility=:minaccessibility&maxaccessibility=:maxaccessibility
+import requests,json
+from colorama import Fore, Style
+import os
+import time
 
-def my_activity(self, master):
-    {
-	"activity": "Take a bubble bath",
-	"accessibility": 0.1,
-	"type": "relaxation",
-	"participants": 1,
-	"price": 0.15,
-}
-    while len(master.bored) < Mas:
-            print(Fore.BLUE if master.id % 2 == 0 else Fore.RED, Style.BRIGHT, end="")
-            clear_screen()
-            my_activity = input(f"{master.name} How about a Relaxing bubble bath? ")
-            if my_activity.lower() == "remove":
-                my_activity = input(f"{master.name} Already had a bubble bath? ").title()
-                if master.release(my_activity):
-                    print(f'lets get you something to do' {my_activity}')
-                    time.sleep(2)
 
-def my_activity2(self, master):
-    {
-	"activity":"Learn a new recipe",
-	"accessibility":0.05,
-	"type":"cooking",
-	"participants":1,
-	"price":0
-}
-/api/activity?accessibility=:accessibility
+class Bored():
+    def __init__(self):
+        self.min_price = 0
+        self.max_price = 0
 
-main()
+    def find_activity():
+        while True:
+            os.system('cls')
+            min_price = input("What is you minimum price? Number between 0 and 1, 'q' to Quit: ")
+            if min_price == 'q':
+                os.system('cls')
+                break
+            os.system('cls')
+            max_price = input("What is you maximum price? Number between 0 and 1, 'q' to Quit: ")
+            if max_price == 'q':
+                os.system('cls')
+                break
+            url= f"http://www.boredapi.com/api/activity?minprice={min_price}&maxprice={max_price}"
+            response = requests.get(url)
+            data = response.json()
+            os.system('cls')
+            print(Fore.BLUE + f'Activity: {data["activity"]}')
+            print(Fore.RED + f'Type: {data["type"]}')
+            print(Fore.YELLOW + f'Participants: {data["participants"]}')
+            print(Fore.GREEN + f'Price: ${data["price"]}')
+            print(Style.RESET_ALL)
+            while True:
+                answer = input("'q' to Quit: ")
+                if answer.lower().strip() == 'q':
+                    break
+                else:
+                    print("Invalid input, please try again")
+            break
+
+    def main():
+        os.system('cls')
+        while True:
+            os.system('cls')
+            answer = input("Would you like to find an activity or quit? 'f' to find activity 'q' to quit: ")
+            if answer.lower().strip() == 'q':
+                os.system('cls')
+                print("Goodbye")
+                break
+            elif answer.lower().strip() == 'f':
+                Bored.find_activity()
+Bored.main()
